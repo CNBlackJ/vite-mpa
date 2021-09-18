3b9d70c6316ebdc5aa538ee5afcfaea23e9704ee
张业生 <zhangyesheng@wps.cn>
2021-09-18 15:07:41 +0800

    multiple page app demo

---
+++ app2/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from '../src/components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>app two</h1>
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin/index.html'),
+        app2: resolve(__dirname, 'app2/index.html')
+      }
+    }
+  }
 })
