89342f0848aa2e366d38a7d2e055a33790b6dae9
张业生 <zhangyesheng@wps.cn>
2021-10-21 17:07:38 +0800

    chore: update layout

--- src/views/Layout/QMenu/index.vue
+++ src/views/Layout/QMenu/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div v-for="(menuGroup, i) in menus" :key="i">
+        <div v-for="(menuGroup, i) in data.menus" :key="i">
             <span>{{ menuGroup.title }}</span>
             <menu-item v-for="(menu, m) in menuGroup.children" :key="m" v-bind="menu"></menu-item>
         </div>
@@ -10,37 +10,30 @@
 <script>
 import MenuItem from './MenuItem.vue'
 
+import { reactive } from 'vue'
+
 export default {
     components: {
         MenuItem
     },
-    data() {
-        return {
+    setup(){
+        const data = reactive({
             menus: [
                 {
                     type: 'online',
                     sort: 1,
                     title: '在线音乐',
-                    children: [
-                        { title: '推荐', icon: '' },
-                        { title: '音乐馆', icon: '' },
-                        { title: '电台', icon: '' },
-                        { title: '视频', icon: '' }
-                    ]
-                },
+                    children: [ { title: '推荐', icon: '' }, { title: '音乐馆', icon: '' }, { title: '电台', icon: '' }, { title: '视频', icon: '' } ]
+                },
                 {
                     type: 'my',
                     sort: 2,
                     title: '我的音乐',
-                    children: [
-                        { title: '我喜欢', icon: '' },
-                        { title: '本地歌曲', icon: '' },
-                        { title: '下载歌曲', icon: '' },
-                        { title: '最近播放', icon: '' }
-                    ]
+                    children: [ { title: '我喜欢', icon: '' }, { title: '本地歌曲', icon: '' }, { title: '下载歌曲', icon: '' }, { title: '最近播放', icon: '' } ]
                 }
             ]
-        }
+        })
+        return { data }
     }
 }
 </script>
